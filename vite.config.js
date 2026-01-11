ca8b0a7cb18a6a191c1fb17a5e82dd8bfa950690
jonckheere <alexander.jonckheere@student.howest.be>
2026-01-11 22:36:25 +0100

    changed back. won't work

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-
+ 
 export default defineConfig({
+
   base: '/integration3/',
 
-  resolve: {
-    alias: {
-      // Force Vite to use Node's crypto, not a browser polyfill
-      crypto: 'node:crypto'
-    }
-  }
 })
+ 
